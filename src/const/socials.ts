6119f13a1a4a2bdd514424efdcbe493cc5765060
Edwin Contreras <ed25gonz@gmail.com>
2025-03-25 22:55:53 -0400

    Add Presentation component and social links; create types for social media to enhance user profile section in the portfolio site

---
+++ src/const/socials.ts
@@ -0,0 +1,45 @@
+import type { Social } from "@/types/social";
+
+import linkedin from "@/assets/svg/linkedin.svg";
+import github from "@/assets/svg/github.svg";
+import mail from "@/assets/svg/mail.svg";
+import cv from "@/assets/svg/cv.svg";
+
+export const SOCIAL: Social[] = [
+  {
+    id: "linkedin",
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/edwin-contreras-1bb516247/",
+    label: "Linkedin",
+    image: {
+      logo: linkedin,
+    },
+  },
+  {
+    id: "github",
+    name: "Github",
+    url: "https://github.com/EdGonzz",
+    label: "Github",
+    image: {
+      logo: github,
+    },
+  },
+  {
+    id: "mail",
+    name: "Mail",
+    url: "mailto:ed25gonz@gmail.com",
+    label: "Mail",
+    image: {
+      logo: mail,
+    },
+  },
+  {
+    id: "cv",
+    name: "CV",
+    url: "https://drive.google.com/file/d/1-0-1-2-3-4-5-6-7-8-9-0/view?usp=sharing",
+    label: "CV",
+    image: {
+      logo: cv,
+    },
+  },
+];
---
+++ src/types/social.ts
@@ -0,0 +1,13 @@
+type SocialId = "linkedin" | "github" | "mail" | "cv";
+
+type SocialName = "Linkedin" | "Github" | "Mail" | "CV";
+
+export interface Social {
+  id: SocialId;
+  name: SocialName;
+  url: string;
+  label: string;
+  image: {
+    logo: any;
+  };
+}
