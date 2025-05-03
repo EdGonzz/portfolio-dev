c78043b0dbb0ea14a5ba99034cd2d007ff664049
Edwin Contreras <ed25gonz@gmail.com>
2025-05-03 01:44:03 -0400

    Agregar nuevos íconos SVG para tecnologías y actualizar el componente de proyectos para incluirlos

--- src/const/PROJECTS.ts
+++ src/const/PROJECTS.ts
@@ -2,6 +2,12 @@ import type { Project } from "@/types/projects";
 
 import github from "@/assets/svg/github.svg";
 import world from "@/assets/svg/world.svg";
+import Astro from "@/assets/svg/astro.svg";
+import tailwind from "@/assets/svg/tailwind.svg";
+import typescript from "@/assets/svg/typescript.svg";
+import javascript from "@/assets/svg/javascript.svg";
+import vercel from "@/assets/svg/vercel.svg";
+import pnpm from "@/assets/svg/pnpm.svg";
 
 export const PROJECTS: Project[] = [
   {
@@ -9,7 +15,7 @@ export const PROJECTS: Project[] = [
     description:
       "Reel Library es una pagina web para navegar entre reseñas y trailers de pelicilas; una manera sencilla de entrar la mejor pelicula para ver. El proyecto demuestra el consumo de APIs y la manipulación del DOM usando JavaScript y TypeScript.",
     image: {
-      src: "https://github.com/EdGonzz/reel-library/raw/main/public/desktop-mockup.webp",
+      src: "https://github.com/EdGonzz/reel-library/raw/main/public/mobile-mockup.webp",
       alt: "Mockup de interfaz móvil de una aplicación llamada “ReelLibrary” para ver información sobre películas. La imagen muestra dos teléfonos inteligentes: el de la izquierda presenta una pantalla de inicio con categorías de géneros cinematográficos, películas populares y una calificación de “Captain America: Brave New World”; el de la derecha muestra una vista detallada de esa misma película con su póster, título, duración (1h 58m), fecha de lanzamiento (14/2/2025), puntuación (6.1/10) y géneros asociados (Acción, Thriller, Ciencia ficción).",
     },
     links: {
@@ -23,12 +29,36 @@ export const PROJECTS: Project[] = [
       },
     },
     tags: [
-      { name: "HTML", classProp: "bg-red-500", logo: github },
-      { name: "CSS", classProp: "bg-blue-500", logo: github },
-      { name: "JavaScript", classProp: "bg-yellow-500", logo: github },
-      { name: "TypeScript", classProp: "bg-blue-400", logo: github },
-      { name: "React", classProp: "bg-blue-600", logo: github },
-      { name: "Vite", classProp: "bg-green-500", logo: github },
+      {
+        name: "Astro",
+        classProp: "bg-[#e03872]/50 border-[#e03872] border-1",
+        logo: Astro,
+      },
+      {
+        name: "Tailwind CSS",
+        classProp: "bg-[#38bdf8]/50 border-[#38bdf8] border-1",
+        logo: tailwind,
+      },
+      {
+        name: "TypeScript",
+        classProp: "bg-[#3178c6]/50 border-[#3178c6] border-1",
+        logo: typescript,
+      },
+      {
+        name: "JavaScript",
+        classProp: "bg-[#f0db4f]/50 border-[#f0db4f] border-1",
+        logo: javascript,
+      },
+      {
+        name: "Vercel",
+        classProp: "bg-[#000000]/50 border-[#000000] border-1",
+        logo: vercel,
+      },
+      {
+        name: "PNPM",
+        classProp: "bg-[#f9ad00]/50 border-[#f9ad00] border-1",
+        logo: pnpm,
+      },
     ],
   },
 ];
